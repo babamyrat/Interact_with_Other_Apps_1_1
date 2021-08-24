c04501f81305717d4e479215ec70ed1177ef3117
Admin <tmstudentler@gmail.com>
2021-08-24 15:13:31 +0300

    step 1

--- app/src/main/java/com/example/interact_with_other_apps_1_1/MainActivity.java
+++ app/src/main/java/com/example/interact_with_other_apps_1_1/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.interact_with_other_apps_1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            Uri number = Uri.parse("tel:5551234");
+            Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+            startActivity(callIntent);
+        });
+
     }
 }
